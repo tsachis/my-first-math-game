09c457334e9fa65387bb96898123452247bdaa17
tshushan <tshushan@outbrain.com>
2017-11-24 22:28:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My first math game
+Math practice for first grade
+
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by tshushan on 24/11/2017.
+ */
+
+class MathGame {
+  constructor() {
+  }
+
+  run() {
+  }
+
+  pause() {
+  }
+}
+
+(function() {
+  const mathGame = new MathGame();
+  mathGame.run();
+})();
